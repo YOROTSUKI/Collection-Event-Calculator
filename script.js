aaddd0fd926ffafe5c6f81ab4580cb97e7a5d919
Tobias Poulsen (Biast12) <tobiaspoulsen@hotmail.com>
2023-12-06 13:40:45 +0100

    Dark theme and theme changer

--- script.js
+++ script.js
@@ -63,7 +63,12 @@ submit.addEventListener(
       leftcoindom.innerHTML = 0;
     }
     needboxdom.innerHTML = needbox;
-    buyskindom.innerHTML = "<span style='color:#FF8C00'>" + buygold + "</span> + <span style='color:#8A2BE2'>" + buypurple + "</span>";
+    buyskindom.innerHTML =
+      "<span style='color:#FF8C00'>" +
+      buygold +
+      "</span> + <span style='color:#8A2BE2'>" +
+      buypurple +
+      "</span>";
     buykbdom.innerHTML = buykb;
     costmetaldom.innerHTML = costmetal;
     leftmetal = metal - costmetal;
@@ -71,3 +76,47 @@ submit.addEventListener(
   },
   false
 );
+
+// Retrieve theme settings from localStorage and system preference
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+// Calculate the initial theme setting
+let currentThemeSetting = calculateSettingAsThemeString({
+  localStorageTheme,
+  systemSettingDark,
+});
+
+// Function to calculate theme setting based on priority
+function calculateSettingAsThemeString({
+  localStorageTheme,
+  systemSettingDark,
+}) {
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
+  }
+
+  return systemSettingDark.matches ? "light" : "dark";
+}
+
+// Event listener for theme toggle button
+const button = document.querySelector("[data-theme-toggle]");
+
+button.addEventListener("click", () => {
+  // Toggle between dark and light themes
+  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+  // Update button text and aria-label
+  const newCta = `Theme: ${
+    newTheme.charAt(0).toUpperCase() + newTheme.slice(1)
+  }`;
+  button.innerText = newCta;
+  button.setAttribute("aria-label", newCta);
+
+  // Update theme attribute on HTML to switch theme in CSS
+  document.querySelector("html").setAttribute("data-theme", newTheme);
+
+  // Update theme setting in local storage and in memory
+  localStorage.setItem("theme", newTheme);
+  currentThemeSetting = newTheme;
+});
